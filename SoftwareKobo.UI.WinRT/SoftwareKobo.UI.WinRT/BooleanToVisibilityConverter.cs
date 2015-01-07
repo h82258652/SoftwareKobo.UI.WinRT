f62960eee2738b78a423564f2bbbf7e4e216decf
h82258652 <842053625@qq.com>
2015-01-07 23:53:21 +0800

    添加两个转换类。

---
+++ SoftwareKobo.UI.WinRT/SoftwareKobo.UI.WinRT/BooleanToVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SoftwareKobo.UI.WinRT
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if ((bool)value)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            Visibility visibility = (Visibility)value;
+            switch (visibility)
+            {
+                case Visibility.Visible:
+                    return true;
+
+                case Visibility.Collapsed:
+                    return false;
+
+                default:
+                    throw new ArgumentException(nameof(value));
+            }
+        }
+    }
+}
